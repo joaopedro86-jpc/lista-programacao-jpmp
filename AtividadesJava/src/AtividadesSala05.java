4ddffad1a056ff6bd9f63d46ad307033dd23510b
joãopedromoreirapessoa <joaopedromoreirapessoa@gmail.com>
2025-08-20 21:18:37 -0300

    Atividades Em Sala

---
+++ AtividadesJava/src/AtividadesSala05.java
@@ -0,0 +1,12 @@
+public class AtividadesSala05 {
+    public static void main(String[] args) {
+        char letra = 'S';
+        char inicial = 'V';
+        String texto = "Code";
+        String temp = "";
+        temp += "" + letra + inicial;
+        System.out.println(temp);
+        System.out.println(temp + texto);
+
+    }
+}
