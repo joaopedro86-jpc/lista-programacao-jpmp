8aeefc0845d0d367236ef10480b4b9a548c1c643
joãopedromoreirapessoa <joaopedromoreirapessoa@gmail.com>
2025-08-26 21:52:28 -0300

    AtividadedeSala

---
+++ AtividadesJava/src/AtividadeSala16.java
@@ -0,0 +1,16 @@
+public class AtividadeSala16 {
+    public static int fatorial(int valor) {
+
+        if (valor == 0)
+            return 1;
+        else
+            return fatorial(valor - 1);
+    }
+    public static void main(String[] args) {
+        
+
+
+    }
+
+}
+
--- AtividadesJava/src/AtividadesSala14.java
+++ AtividadesJava/src/AtividadesSala14.java
@@ -8,5 +8,6 @@ public class AtividadesSala14 {
         System.out.println(valor);
     }
     public static void main(String[] args){}
-    }
 
+
+    }
