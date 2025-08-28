1cf9faae2c313fcaa4f56aa17f7f0ac17da2671d
joãopedromoreirapessoa <joaopedromoreirapessoa@gmail.com>
2025-08-28 19:31:25 -0300

    Atividadesala

---
+++ AtividadesJava/src/Atividade2.java
@@ -0,0 +1,10 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+        int dia =1;
+        String mes ="Junho";
+        String ano ="2008";
+        System.out.println("Dia: "+dia);
+        System.out.println("Mes: "+mes);
+        System.out.println("Ano: "+ano);
+    }
+}
