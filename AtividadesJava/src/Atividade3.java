89935b354d1de5787c9d067d95d150bc15eb8b42
joãopedromoreirapessoa <joaopedromoreirapessoa@gmail.com>
2025-08-28 19:38:55 -0300

    Atividadesala

---
+++ AtividadesJava/src/Atividade3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Atividade3 {
+    public static void main(String[] args) {
+        int qtdAlunos = 20;
+        double mediaTurma = 78;
+        boolean aprovado = true;
+        String professor = "Victor";
+        System.out.println("Quantidade de alunos: "+qtdAlunos);
+        System.out.println("Quantidade de turmas: "+mediaTurma);
+        System.out.println("Quantidade de aprovados: "+aprovado);
+        System.out.println("Quantidade de professor: "+professor);
+
+
+    }
+}
