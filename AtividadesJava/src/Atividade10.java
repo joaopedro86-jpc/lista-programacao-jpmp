43d4bcb1192a8d053911f99a5bc8e7be7d022d53
joãopedromoreirapessoa <joaopedromoreirapessoa@gmail.com>
2025-08-29 19:26:13 -0300

    Inicialização da estrutura do projeto

--- AtividadesJava/src/Atividade10.java
+++ AtividadesJava/src/Atividade10.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
 public class Atividade10 {
-    public static void main(String[] args)
-        {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int valor = 0, contador = 0;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro para saber sua tabuada");
+        valor = leia.nextInt();
+
+        while (contador<=10){
+            System.out.println(valor + "*" + contador + " = " + (valor*contador));
+            contador++;
+        }
     }
+
 }
