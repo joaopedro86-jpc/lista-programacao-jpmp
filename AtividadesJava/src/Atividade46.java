9cad4b8e4fd427b0f974e9caf78ea0abe7889cd1
joãopedromoreirapessoa <joaopedromoreirapessoa@gmail.com>
2025-09-04 21:51:54 -0300

    Atividadesala

--- AtividadesJava/src/Atividade46.java
+++ AtividadesJava/src/Atividade46.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class Atividade46 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int[][] a = new int[2][2];
         int soma = 0;
         System.out.println("Digite os numeros para serem armazenados");
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
-                a[i][j] = scanner.nextInt();
             }
 
         }
